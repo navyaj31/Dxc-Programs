fa67458394af62b93c3048db5feb3ad2cb4e1f4e
Jangam <navya.jangam@dxc.com>
2023-04-28 14:25:53 +0530

    updated

---
+++ Interfacevehicle/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Interfacevehicle
+{
+    interface IVehiculo
+    {
+        void Drive();
+        bool Refuel(int value);
+    }
+
+    class Car : IVehiculo
+    {
+        int gasoline;
+        public Car(int Gasoline)
+        {
+            gasoline = Gasoline;
+        }
+
+        public void Drive()
+        {
+            if (gasoline > 0)
+            {
+                Console.WriteLine("The car is driving.");
+            }
+            else
+            {
+                Console.WriteLine("The car is not driving.");
+            }
+        }
+        public bool Refuel(int gasolineToAdd)
+        {
+            gasoline += gasolineToAdd;
+            return true;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the amount of gasoline to refuel: ");
+            int gasolineToAdd = int.Parse(Console.ReadLine());
+            Car myCar = new Car(0);
+            myCar.Refuel(gasolineToAdd);
+            myCar.Drive();
+        }
+    }
+}
---
+++ pushpop/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace pushpop
+{
+    class MyStack<T>
+    {
+        public int count;
+        public T[] array;
+        public MyStack()
+        {
+            array = new T[15];
+            count = 0;
+        }
+
+        public void push(T obj)
+        {
+            array[count] = obj;
+            count++;
+        }
+
+        public T pop()
+        {
+            T item = array[count - 1];
+            count--;
+            return item;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int j = 0;
+            int z = 0;
+
+            // int
+            MyStack<int> myStack = new MyStack<int>();
+            myStack.push(1);
+            myStack.push(2);
+            myStack.push(3);
+            myStack.push(4);
+            myStack.push(5);
+            myStack.push(6);
+
+            foreach (int i in myStack.array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"Number of objects in the stack = {myStack.count}");
+            Console.WriteLine($"Poped item-{++j} is: {myStack.pop()}");
+            Console.WriteLine($"Poped item-{++j} is: {myStack.pop()}");
+            // string
+            MyStack<string> myStack1 = new MyStack<string>();
+            myStack1.push("one");
+            myStack1.push("two");
+            myStack1.push("three");
+            myStack1.push("four");
+            myStack1.push("five");
+
+            foreach (string i in myStack1.array)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine($"Number of objects in the stack = {myStack1.count}");
+            Console.WriteLine($"Poped item-{++z} is: {myStack1.pop()}");
+            Console.WriteLine($"Poped item-{++z} is: {myStack1.pop()}");
+        }
+    }
+}
+
